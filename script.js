4243c5c1bb868aeb6db1c2dcc516f81592b20673
KGazica <kresimir.gazica@gmail.com>
2022-11-14 19:38:53 +0100

    finished task

---
+++ script.js
@@ -0,0 +1,5 @@
+function showtext() {
+  document.getElementById("new").innerHTML =
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat veniam corrupti voluptas facilis? Quidem necessitatibus delectus voluptas aliquam eos vero similique, aut illo quaerat nesciunt commodi excepturi nostrum? Repudiandae, corporis?";
+  document.getElementById("click").style.display = "none";
+}
